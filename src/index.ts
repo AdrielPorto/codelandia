22850de0753432f83c2dc0ac03def6662c1f6fc2
Adriel Porto <adriel.porto@dmslog.com>
2022-11-23 02:46:57 -0300

    super teste

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,15 @@ import errorHandler from './middlewares/error-handle.middlleware';
 import authorizationRoute from './routes/authorization.routes';
 
 const cors = require('cors');
-// Importing the fs and https modules -------------- STEP 1
 const https = require("https");
 const fs = require("fs");
 
 const app = express();
 
-// Init Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// Mount express-sanitizer middleware here
 
-// Server static html file to check if the server is working
 app.use("/", express.static("public"));
 const options = {
     key: fs.readFileSync("./cert.key"),
@@ -25,12 +21,11 @@ const options = {
 };
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.all('*', function (req, res) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     app.use(cors());
-    next();
 })
 
 app.use(usersRoute);
@@ -43,8 +38,6 @@ const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
-
-
 https.createServer(options, app).listen(3000, () => {
     console.log(`HTTPS server started on port 8080`);
 });
