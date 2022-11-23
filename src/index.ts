d19cc2d7a32bc547f62de0c11df61b6cd263397a
Adriel Porto <adriel.porto@dmslog.com>
2022-11-23 02:39:13 -0300

    super teste

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,36 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import usersRoute from './routes/users.routes';
 import statusRoute from './routes/status.routes';
 import errorHandler from './middlewares/error-handle.middlleware';
 import authorizationRoute from './routes/authorization.routes';
 
-const app = express();
 const cors = require('cors');
+// Importing the fs and https modules -------------- STEP 1
+const https = require("https");
+const fs = require("fs");
 
+const app = express();
+
+// Init Middleware
 app.use(express.json());
-app.use(cors({ credentials: true, origin: `http://44.202.38.5:3000`, optionsSuccessStatus: 200 }))
+app.use(express.urlencoded({extended: true}));
+
+// Mount express-sanitizer middleware here
+
+// Server static html file to check if the server is working
+app.use("/", express.static("public"));
+const options = {
+    key: fs.readFileSync("./cert.key"),
+    cert: fs.readFileSync("./cert.crt"),
+};
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  app.use(cors());
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    app.use(cors());
+    next();
 })
 
 app.use(usersRoute);
@@ -23,6 +38,13 @@ app.use(statusRoute);
 app.use(errorHandler);
 app.use(authorizationRoute);
 
-app.listen(3000, () => {
-  console.log('Estou funcionando');
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+
+https.createServer(options, app).listen(3000, () => {
+    console.log(`HTTPS server started on port 8080`);
 });
