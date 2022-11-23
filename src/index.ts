f946a3c6adda7ebd82c407cba78603413b055131
Adriel Porto <adriel.porto@dmslog.com>
2022-11-23 20:00:26 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,21 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import usersRoute from './routes/users.routes';
 import statusRoute from './routes/status.routes';
 import errorHandler from './middlewares/error-handle.middlleware';
 import authorizationRoute from './routes/authorization.routes';
 
-const cors = require('cors');
-const https = require("https");
-const fs = require("fs");
-
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(cors({ credentials: true, origin: `http://127.0.0.1:3000`, optionsSuccessStatus: 200 }))
 
-
-app.use("/", express.static("public"));
-const options = {
-    key: fs.readFileSync("./cert.key"),
-    cert: fs.readFileSync("./cert.crt"),
-};
-app.use(express.json());
-
-app.all('*', function (req, res) {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     app.use(cors());
+    next();
 })
 
 app.use(usersRoute);
@@ -33,11 +23,6 @@ app.use(statusRoute);
 app.use(errorHandler);
 app.use(authorizationRoute);
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
-https.createServer(options, app).listen(3000, () => {
-    console.log(`HTTPS server started on port 8080`);
+app.listen(3000, () => {
+    console.log('Estou funcionando');
 });
